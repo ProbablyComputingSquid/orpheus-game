bed34aba26e5b002d7c606f93c6bdc3ea88d548d
Jacob <66024281+ProbablyComputingSquid@users.noreply.github.com>
2024-08-01 19:09:05 +0000

    spent half an hour trying to get something to work
    but it didn't work :(

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+
 /* Made using orpheus framework from hack club (thanks!)
    Produced for the joshco game jam https://itch.io/jam/joshco-game-jam
 */
-
 // system prompt
+
+/* TODO: find out how to do put this in js
+curl -f -sS "https://api.stability.ai/v2beta/stable-image/generate/ultra" \
+  -H "authorization: Bearer sk-MYAPIKEY" \
+  -H "accept: image/*" \
+  -F prompt="Lighthouse on a cliff overlooking the ocean" \
+  -F output_format="webp" \
+  -o "./lighthouse.webp"
+  */
 var myGame = new WizardOrpheus('', `
 The user is trying to complete their game for the Official JoshCo Game Jam. They've had about a month to complete their game, but they have been procrastinating, and have put it off until the very last day. You decide what happens as the user tries to complete their game in time. Keep everything realistic.   
 `)
@@ -11,6 +20,8 @@ The user is trying to complete their game for the Official JoshCo Game Jam. They
 myGame.variable('completion', 'How complete the user\'s game for the game jam is. From 0 (not even started) to 100 (fully complete)', 0)
 
 myGame.variable('timeLeft', 'How much time (in hours) the user has until the game jam is over. Decreases when user spends time doing things. From 24 (just started) to 0 (submission time)', 24)
+
+myGame.variable('imagePrompt', 'Describe the image that should be generated. The image should symbolize what is happening.', "")
 
 // my code is self-documenting
 myGame.createUserAction({
@@ -42,7 +53,7 @@ myGame.botAction('respond', 'Send a text response to the user', { message: 'What
   
   document.getElementById('completion').innerHTML = data.currentVariables.completion.value
   document.getElementById('timeLeft').innerHTML = data.currentVariables.timeLeft.value
-
+  
   // [TODO] - implement this
 
   //document.body.style.backgroundColor = `rgba(0, 0, 0, ${data.currentVariables.tiredLevel.value / 100})`
